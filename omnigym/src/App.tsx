a2af971e32e326404e24107cb932088421708962
Violet Yousif <violetyousif@gmail.com>
2025-02-18 02:48:27 -0500

    Adding Roberts home & registergym pages.

--- omnigym/src/App.tsx
+++ omnigym/src/App.tsx
@@ -1,5 +1,4 @@
-
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import {
   IonApp,
   IonIcon,
@@ -15,10 +14,11 @@ import { personOutline, clipboardOutline, settingsOutline } from 'ionicons/icons
 import Profile from './pages/Profile';
 import Routine from './pages/Routine';
 import Settings from './pages/Settings';
+import Home from './pages/Home';  // Import your Home page
+import RegisterGym from './pages/RegisterGym'; // Import the RegisterGym page
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
 
 /* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
@@ -32,7 +32,7 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-// import './App.scss'
+
 /**
  * Ionic Dark Mode
  * -----------------------------------------------------
@@ -42,48 +42,56 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+import '@ionic/react/css/palettes/dark.system.css';  // For dark mode
 
 /* Theme variables */
 import './theme/variables.css';
 
+
 setupIonicReact();
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
+    <IonApp>
+      <IonReactRouter>
         <IonRouterOutlet>
-          <Route exact path="/Profile">
-            <Profile />
-          </Route>
-          <Route exact path="/Routine">
-            <Routine />
-          </Route>
-          <Route path="/Settings">
-            <Settings />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/Profile" />
-          </Route>
+          <Switch>
+            {/* Routes where the tab bar should NOT be shown */}
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/register-gym" component={RegisterGym} />
+
+            {/* Routes where the tab bar SHOULD be shown */}
+            <Route>
+              <IonTabs>
+                <IonRouterOutlet>
+                  <Route exact path="/profile" component={Profile} />
+                  <Route exact path="/routine" component={Routine} />
+                  <Route exact path="/settings" component={Settings} />
+                  <Route exact path="/">
+                    <Redirect to="/profile" /> {/* Default to Profile if no route is found */}
+                  </Route>
+                </IonRouterOutlet>
+
+                {/* Tab Bar (Only shown for Profile, Routine, and Settings) */}
+                <IonTabBar slot="bottom">
+                  <IonTabButton tab="profile" href="/profile">
+                    <IonIcon aria-hidden="true" icon={personOutline} />
+                    <IonLabel>Profile</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab="routine" href="/routine">
+                    <IonIcon aria-hidden="true" icon={clipboardOutline} />
+                    <IonLabel>Routine</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab="settings" href="/settings">
+                    <IonIcon aria-hidden="true" icon={settingsOutline} />
+                    <IonLabel>Settings</IonLabel>
+                  </IonTabButton>
+                </IonTabBar>
+              </IonTabs>
+            </Route>
+          </Switch>
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="profile" href="/Profile">
-            <IonIcon aria-hidden="true" icon={personOutline} />
-            <IonLabel>Profile</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="routine" href="/Routine">
-            <IonIcon aria-hidden="true" icon={clipboardOutline} />
-            <IonLabel>Routine</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="Settings" href="/Settings">
-            <IonIcon aria-hidden="true" icon={settingsOutline} />
-            <IonLabel>Settings</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
+      </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
