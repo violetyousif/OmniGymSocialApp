027882bd8175aff27ae0edec19550861b7301ef7
Violet Yousif <95271451+violetyousif@users.noreply.github.com>
2025-04-01 13:16:05 -0400

    Update settings.py

--- Fullstack/Backend/config/settings.py
+++ Fullstack/Backend/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vovpvdz8z!fxlf6_c%qq6t_==ifcfl19rc9i-(dmd=q99uk3=&'
+SECRET_KEY = DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
