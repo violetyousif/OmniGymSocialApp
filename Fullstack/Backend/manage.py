bf48d2d479f8d9d98aec38b9db43a9a7fb1874fe
Violet Yousif <violetyousif@gmail.com>
2025-04-07 03:45:20 -0400

    Removing old code

--- Fullstack/Backend/manage.py
+++ Fullstack/Backend/manage.py
@@ -1,11 +1,33 @@
+# #!/usr/bin/env python
+# """Django's command-line utility for administrative tasks."""
+# import dotenv
+# import os
+# import sys
+# # dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+# dotenv.load_dotenv()
+
+
+# def main():
+#     """Run administrative tasks."""
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import dotenv
 import os
 import sys
-# dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
-dotenv.load_dotenv()
-
 
 def main():
     """Run administrative tasks."""
@@ -19,7 +41,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
